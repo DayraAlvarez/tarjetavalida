e213320fb390070e85d30ce7e4f43a6135b6608a
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-04-11 18:27:48 +0000

    Se agregaron lineas de codigo

---
+++ app.js
@@ -0,0 +1,12 @@
+//Inicia programa pidiendo input a usuario por medio de prompt
+var cardNumber = prompt ('Ingresa el numero de tarjeta');
+
+var validNumber = function (plasticNumber){
+  var str=numCard.toString ();
+  var arr=str.split ('');
+  arr.reverse();
+  var prod=1;
+  var sum=0;
+
+  for(var i=o; i<arr.lenght;i++){
+  
--- js/app.js
+++
@@ -1,2 +0,0 @@
-//Inicia programa pidiendo input a usuario por medio de prompt
-var cardNumber = prompt ('Ingresa el numero de tarjeta');
